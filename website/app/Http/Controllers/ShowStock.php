fadfda800ba4b4a08adb0bba63cb8fb6b8fb5c00
Paul <paul.s.davidson26@gmail.com>
2017-03-31 10:53:19 +1100

    changed readme

--- website/app/Http/Controllers/ShowStock.php
+++ website/app/Http/Controllers/ShowStock.php
@@ -10,6 +10,7 @@ class ShowStock extends Controller
 	public function __invoke($code) {
 		$data = Stock::where('stock_symbol', $code)->get();
 		// $data = Stock::all();
+		// print_r(array_keys(json_decode($data[0]->history, true)));
 		return view('stock', ['stock' => $data[0]]);
 	}
 }
