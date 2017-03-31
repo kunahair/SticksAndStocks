472f837308f61196ba40a5abdd7080fce17f9a1e
Josh Gerlach <s3453952@student.rmit.edu.au>
2017-04-01 10:59:13 +1100

    Added API call to get a list of all stock name and symbols for searching.

---
+++ website/app/Http/Controllers/DatabaseStockCodes.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Stock;
+use App\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+
+header('Access-Control-Allow-Origin: *');
+
+class DatabaseStockCodes extends Controller
+{
+    /**
+     * Get all stock listings. Gets Stocks Symbol and Stock Name
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Symfony\Component\HttpFoundation\Response
+     */
+    public function getAllStockCodesAndCompanyNames()
+    {
+        //Call the stocks table and get all stock names and symbold
+        $stockListing = DB::table('stocks')->select('stock_name', 'stock_symbol')->get();
+        //Return JSON array of data with status code 200
+        return response($stockListing, 200);
+    }
+
+}
