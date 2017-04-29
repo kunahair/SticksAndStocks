d2f3ed674f20b7397063b60def433c5bd7dceda6
Woooooo! <s3505307@student.rmit.edu.au>
2017-04-29 23:49:17 +1000

    made changes to profiles page

--- website/resources/views/profiles.blade.php
+++ website/resources/views/profiles.blade.php
@@ -3,20 +3,55 @@
 
 @section('title','Pineapple')
 
+
+<style>
+
+
+
+
+</style>
+
+
+
     <h1>All Users</h1>
     {{--Padding--}}
     <div class="col-xs-1 col-md-3"></div>
 
-    <div class="col-xs-10 col-md-6">
-        <table class="col-xs-4 table table-hover">
-            {{--Loop through all users (not admins) and show in the table--}}
-            @foreach($users as $user)
-                <tr onclick="window.document.location=' {{url('profile')}}/{{$user["id"]}}'">
-                    <td>{{$user["name"]}}</td>
-                </tr>
+    <div class="table">
+        <thead>
+        <tr>
+            <th>Name</th>
+            <tr>
+            </thead>
+
+        <tbody>
+        <tr>
+            <th scope=""row"></th>
+            <td>
+
+        {{--Loop through all users (not admins) and show in the table--}}
+        @foreach($users as $user)
+            <tr onclick="window.document.location=' {{url('profile')}}/{{$user["id"]}}'">
+                <td>{{$user["name"]}}</td>
+            </tr>
             @endforeach
 
-        </table>
+
+            </td>
+
+
+        </tr>
+
+        </tbody>
+
+
+
+        </tr>
+        </thead>
+
+
+
+
     </div>
 
     {{--Padding--}}
