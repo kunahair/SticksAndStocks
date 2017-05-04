5873d8955377984d00682f22ef160cae51d2122b
Woooooo! <s3505307@student.rmit.edu.au>
2017-05-05 09:59:01 +1000

    css for friends page

--- website/resources/views/friends.blade.php
+++ website/resources/views/friends.blade.php
@@ -3,12 +3,28 @@
 
 @section('title','Pineapple')
 
-    <h1>Friends</h1>
+<style>
+
+    thead{
+        background-color: black;
+    }
+
+</style>
+
+
+
+<h1>Friends</h1>
     {{--Padding--}}
     <div class="col-xs-1 col-md-3"></div>
 
-    <div class="col-xs-10 col-md-6">
-        <table class="col-xs-4 table table-hover">
+        <table class="col-xs-4 table table-hover table-bordered">
+
+            <thead>
+            <tr>
+                <th>Name</th>
+            </tr>
+            </thead>
+            <tbody>
 
             {{--Loop through every friend and show in the table--}}
             @foreach($friends as $friend)
@@ -17,8 +33,10 @@
                 </tr>
             @endforeach
 
+            <tbody>
+
         </table>
-    </div>
+    
 
     {{--Padding--}}
     <div class="col-xs-1 col-md-3"></div>
