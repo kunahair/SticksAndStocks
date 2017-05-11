f5abe53f71ed535b957d0675a1d9eaccfbac9443
Woooooo! <s3505307@student.rmit.edu.au>
2017-05-12 01:39:52 +1000

    updated contact page

--- website/resources/views/contact.blade.php
+++ website/resources/views/contact.blade.php
@@ -2,21 +2,67 @@
 
 @section('title','Pineapple')
 
-<body>
+<style>
+    .container{
+
+        background-color: #222;
+        color:#fff;
+        margin-top:30px;
+        padding-top:30px;
+    }
+
+
+  .form-control{
+
+      color:blue;
+  }
+
+
+    #map{
+        margin-top:30px;
+        padding-top:30px
+
+    }
+
+</style>
+
 
     @include('layouts.header-navbar')
 
     <link href="{{ url('css/broken_css_pages.css') }}" rel="stylesheet" type="text/css">
 
-    <div class=" font-color">
-        <div class="col-md-3 col-md-offset-4" ><h1>Contact Us</h1>
-            <ul class="list-group">
-                <li class="list-group-item">SticksAndStocks Ltd Pty</li>
-                <li class="list-group-item">PH:963424</li>
-                <li class="list-group-item">Address: 435-457 Swanston St, Melbourne VIC 3000</li>
-            </ul>
 
-            <div id="map" style="width:400px;height:400px;"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-5">
+                <p>Contact us and we'll get back to you within 24 hours.</p>
+                <p><span class="glyphicon glyphicon-map-marker"></span> Melbourne, Australia</p>
+                <p><span class="glyphicon glyphicon-phone"></span> +00 1515190097</p>
+                <p><span class="glyphicon glyphicon-envelope"></span> uniquedrives@something.com</p>
+            </div>
+            <div class="col-sm-7">
+                <div class="row">
+                    <div class="col-sm-6 form-group">
+                        <input class="form-control" id="name" name="name" placeholder="Name" type="text" required>
+                    </div>
+                    <div class="col-sm-6 form-group">
+                        <input class="form-control" id="email" name="email" placeholder="Email" type="email" required>
+                    </div>
+                </div>
+                <textarea class="form-control" id="comments" name="comments" placeholder="Description" rows="5"></textarea><br>
+                <div class="row">
+                    <div class="col-sm-12 form-group">
+                        <button class="btn btn-default pull-right" type="submit">Send</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+    <div id="map" style="width:400px;height:400px;"></div>
             <script>
                 function initMap() {
                     var uluru = {lat: -37.8084349, lng: 144.9630036};
