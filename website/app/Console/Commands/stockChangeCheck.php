62185cd6623f16d3a3d13ba69336153f5b732105
Josh Gerlach <s3453952@student.rmit.edu.au>
2017-06-02 14:36:21 +1000

    subject: Updated the stock checker to get the latest price *Stock price updates where up to an hour behind the actual price. Function updated to get the actual latest price from API. *Stock price updates was making multiple calls to the database that where not required, collapsed into single database call and information extracted from ORM.

--- website/app/Console/Commands/stockChangeCheck.php
+++ website/app/Console/Commands/stockChangeCheck.php
@@ -50,6 +50,9 @@ class stockChangeCheck extends Command
         $users = User::all();
         $setPercentage = 2;
 
+        $currencyConverver = new \CurrencyConverter;
+        $exchangeRate = $currencyConverver->USDtoAUD(1.00);
+
         foreach ($users as $user) {
 
             $buySell = Array();
@@ -70,10 +73,30 @@ class stockChangeCheck extends Command
                 foreach ($buySell as $key => $value) {
                     // Check if the user has the stock
                     if ($value > 0) {
-                        $stock_name = Stock::where('id', $key)->first()->stock_name;
-                        $stock_symbol = Stock::where('id', $key)->first()->stock_symbol;
-                        // Current Value
-                        $currentPrice = Stock::where('id', $key)->first()->current_price;
+                        //Get stock from Database
+                        $stock = Stock::where('id', $key)->first();
+                        //Get Stock name
+                        $stock_name = $stock->stock_name;
+                        //Get Stock symbol/code
+                        $stock_symbol = $stock->stock_symbol;
+
+                        $currentPrice = $stock->current_price;
+
+                        //Get the latest price of the selected Stock
+                        $currentCompanyInfo = new \CurrentCompanyStockInformation;
+                        $stockInfo = $currentCompanyInfo->currentDetails($stock_symbol, $stock->market);
+
+                        //If the latest Stock information is not null, set as current price and update the database
+                        if ($stockInfo != null)
+                        {
+                            //If the Stock is not ASX stock, convert from US to AUD
+                            if($stock->market != 'ASX')
+                                $currentPrice = $stockInfo["curr_price"]["price"] * $exchangeRate;
+
+                            //Set the current price in the database and save
+                            $stock->current_price = $currentPrice;
+                            $stock->save();
+                        }
 
                         // Append to the Portfolio Value
                         $user->portfolio += $currentPrice * $value;
