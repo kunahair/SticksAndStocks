c5c5d63d0ce795f8455cab0b4d95c6b0b0f7d69a
Josh Gerlach <s3453952@student.rmit.edu.au>
2017-06-05 20:15:54 +1000

    subject: Updated Trade Account functions *Did minor changes to Trade Account functions that where violating the Single Responsibility principle

--- website/app/TradeAccount.php
+++ website/app/TradeAccount.php
@@ -29,18 +29,7 @@ class TradeAccount extends Model
         //Holder for grouped transactions
         $transactions = array();
 
-        //Loop through all the transactions the current trade account has
-        //Group all the transactions into the transactions array
-        foreach ($this->transactions as $transaction)
-        {
-            //If the stock has not been assigned into transactions, add it
-            if(!array_key_exists($transaction->stock_id, $transactions))
-            {
-                $transactions[$transaction->stock_id] = array();
-            }
-            //Add the current transaction to its transactions group
-            array_push($transactions[$transaction->stock_id], $transaction);
-        }
+        $this->groupTransactions($transactions);
 
         $allStocksTotalValue = 0.00;
         $allStocksTotalCount = 0;
@@ -91,9 +80,6 @@ class TradeAccount extends Model
 
                 //$stock_total_growth += ($stock_total_cost * ($transaction->bought - $transaction->sold));
 
-                //echo '<pre>';
-                //print_r($transaction->price);
-                //echo '</pre>';
             }
 
             //If the stock owned is less than 1 (it should never hit below 0)
@@ -106,23 +92,6 @@ class TradeAccount extends Model
             //Calculate the total amount of growth that the account has for this stock (overall NOT average)
             $stock_total_growth = ($stock_total_cost / ($stock_owned - $stock_sold)) - $stock_current_price;
 
-//            if ($stock_total_growth > 0.00)
-//                $stock_total_growth *= -1;
-            //$stock_total_growth *= ($stock_owned - $stock_sold) * -1.00;
-
-            //Get the growth as a percentage
-//            $stock_total_growth_percentage = ((($stock_current_price / ($stock_total_cost / ($stock_owned - $stock_sold))) * 100) - 100) * -1;
-
-            //Add stock information to the holding table
-//            echo '<tr>
-//                                    <td class="col-xs-1 " style="padding: 0px"><a href="' . "../stock/". $stock_symbol . '">' . $stock_symbol . '</a></td>
-//                                    <td class=col-xs-4" style="padding: 0px">' . $stock_name . '</td>
-//                                    <td class=col-xs-1" style="padding: 0px">$' . number_format($stock_total_cost, 2) . '</td>
-//                                    <td class=col-xs-1" style="padding: 0px">$' . number_format($stock_current_price, 2) . '</td>
-//                                    <td class=col-xs-2" style="padding: 0px">$' . number_format($stock_total_growth, 2) . ' (' . number_format($stock_total_growth_percentage, 2) . '%)' . '</td>
-//                                    <td class=col-xs-2" style="padding: 0px">' . ($stock_owned - $stock_sold) . '</td>
-//                                    <td class=col-xs-1" style="padding: 0px">' . '<a href="#">view</a>' . '</td>
-//                                 </tr>';
 
             $allStocksTotalValue += $stock_current_price * ($stock_owned - $stock_sold);
             $allStocksTotalCount += ($stock_owned - $stock_sold);
@@ -143,18 +112,7 @@ class TradeAccount extends Model
 
         $groupedStocks = array();
 
-        //Loop through all the transactions the current trade account has
-        //Group all the transactions into the transactions array
-        foreach ($this->transactions as $transaction)
-        {
-            //If the stock has not been assigned into transactions, add it
-            if(!array_key_exists($transaction->stock_id, $transactions))
-            {
-                $transactions[$transaction->stock_id] = array();
-            }
-            //Add the current transaction to its transactions group
-            array_push($transactions[$transaction->stock_id], $transaction);
-        }
+        $this->groupTransactions($transactions);
 
         $allStocksTotalValue = 0.00;
         $allStocksTotalCount = 0;
@@ -206,9 +164,6 @@ class TradeAccount extends Model
 
                 //$stock_total_growth += ($stock_total_cost * ($transaction->bought - $transaction->sold));
 
-                //echo '<pre>';
-                //print_r($transaction->price);
-                //echo '</pre>';
             }
 
             //If the stock owned is less than 1 (it should never hit below 0)
@@ -241,16 +196,6 @@ class TradeAccount extends Model
             $groupedStocks[$stock_symbol]["total_growth_percentage"] = number_format($stock_total_growth_percentage, 2);
             $groupedStocks[$stock_symbol]["owns"] = ($stock_owned - $stock_sold);
 
-            //Add stock information to the holding table
-//            echo '<tr>
-//                                    <td class="col-xs-1 " style="padding: 0px"><a href="' . "../stock/". $stock_symbol . '">' . $stock_symbol . '</a></td>
-//                                    <td class=col-xs-4" style="padding: 0px">' . $stock_name . '</td>
-//                                    <td class=col-xs-1" style="padding: 0px">$' . number_format($stock_total_cost, 2) . '</td>
-//                                    <td class=col-xs-1" style="padding: 0px">$' . number_format($stock_current_price, 2) . '</td>
-//                                    <td class=col-xs-2" style="padding: 0px">$' . number_format($stock_total_growth, 2) . ' (' . number_format($stock_total_growth_percentage, 2) . '%)' . '</td>
-//                                    <td class=col-xs-2" style="padding: 0px">' . ($stock_owned - $stock_sold) . '</td>
-//                                    <td class=col-xs-1" style="padding: 0px">' . '<a href="#">view</a>' . '</td>
-//                                 </tr>';
 
             $allStocksTotalValue += $stock_current_price * ($stock_owned - $stock_sold);
             $allStocksTotalCount += ($stock_owned - $stock_sold);
@@ -267,24 +212,29 @@ class TradeAccount extends Model
 
         $groupedStocks["stats"]["total_stock_value"] = number_format($allStocksTotalValue, 2);
 
-//        echo '</tbody></table>';
-//
-//        //Show the average Stock value of this Trade Account
-//        echo '<div class="col-xs-12" style="padding-left: 0">';
-//
-//        if ($allStocksTotalCount > 0)
-//            echo '<h4>Stock Average Value: $' . number_format(($allStocksTotalValue / $allStocksTotalCount),2) . 'AUD</h4>';
-//
-//        echo '</div>';
-//
-//        //Show the total Stock value of this Trade Account
-//        echo '<div class="col-xs-12" style="padding-left: 0">';
-//
-//        echo '<h4>Stock Total Value: $' . number_format($allStocksTotalValue, 2) . 'AUD</h4>';
-//
-//        echo '</div>';
 
         return $groupedStocks;
+    }
+
+    /**
+     * Group all the transactions into the transactions array
+     * @param $transactions - Array where grouped transactions are to be stored
+     */
+    private function groupTransactions(&$transactions)
+    {
+        //Loop through all the transactions the current trade account has
+        //Group all the transactions into the transactions array
+        foreach ($this->transactions as $transaction)
+        {
+            //If the stock has not been assigned into transactions, add it
+            if(!array_key_exists($transaction->stock_id, $transactions))
+            {
+                $transactions[$transaction->stock_id] = array();
+            }
+            //Add the current transaction to its transactions group
+            array_push($transactions[$transaction->stock_id], $transaction);
+        }
+
     }
 
 }
